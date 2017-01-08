a3a407c8481557f9a91e74d1c8a9af7b5b39a441
Sujeong Yeom <lalalaflow@gmail.com>
2017-01-08 15:18:00 +0900

    TraderCaller 파일의 yarbong 을 lalalaflow로 수정

--- src/main/java/supersimplestocks/TraderCaller.java
+++ src/main/java/supersimplestocks/TraderCaller.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by yarbong on 21/04/2016.
+ * Created by lalalaflow on 21/04/2016.
  */
 public class TraderCaller {
     private List<MainTrader> mainTraders;
